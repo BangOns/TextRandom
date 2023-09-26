e3f3b3058f38b535add50512a3d327d0fee01e9b
BangOns <syahr9950@gmail.com>
2023-09-26 10:32:57 +0700

    updating

--- app/api/GetFetch.jsx
+++ app/api/GetFetch.jsx
@@ -1,4 +1,6 @@
 export async function GetFetch(id) {
-  const response = await fetch(`http://localhost:3000/api/user?_pages=${id}`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_pages=${id}&_limit=10`
+  );
   return response.json();
 }
